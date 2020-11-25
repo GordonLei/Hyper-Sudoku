be244fd461b7cea7776203451b656943bfc581aa
Gordon Lei <gl1776@nyu.edu>
2020-11-25 18:26:51 -0500

    added print method and reading/writing files

--- hyperSudoku.py
+++ hyperSudoku.py
@@ -1,12 +1,30 @@
-class sudokuPuzzle():
+class SudokuPuzzle():
     #constructor 
     def __init__(self):
         self.coordinateValue = [[0 for i in range(9)] for j in range(9)]
         self.coordinateDomains = {}
-    #
+        self.input_file = ""
+        self.output_file = ""
+    #create the puzzle and give it the correct input and output files
+    def create_puzzle(self, inputObj, outputObj):
+        #coordinateArray = []
+        input_lines = inputObj.readlines() 
+        for i in range(9):
+            temp_num_list = input_lines[i].strip().split()
+            #coordinateArray.append(temp_num_list)
+            self.coordinateValue[i] = temp_num_list
+        
+        self.input_file = inputObj
+        self.output_file = outputObj
+    def print_solution(self):
+        for row in self.coordinateValue:
+            line = " ".join(row)
+            print(line)
+            self.output_file.write(line + "\n")
+    #check if this is in the overlapping regions
     def checkInOverlappingBlock(self,x,y):
         return ((0<x<=3 or 3<x<=7) and (0<y<=3 or 3<y<=7))
-    #
+    #forward checking algorithmn
     def forwardCheck(self, x, y, coord_value):
         if coord == 0:
             return
@@ -39,18 +57,18 @@ class sudokuPuzzle():
             #remove domain in overlapping block
             if (self.checkInOverlappingBlock(x,y)):
                 startx = x - (x % 3) + 1
-        endx = startx + 3
-        starty = y - (y % 3) + 1
-        endy = starty + 3
-        for i in range(startx, endx, 1):
-                        for j in range(starty, endy, 1):
-                                temp_arr = self.coordinateDomains[str(i) + str(j)]
-                                temp_arr.remove(coord_value)
-                                self.coordinateDomains[str(i) + str(j)] = temp_arr
+                endx = startx + 3
+                starty = y - (y % 3) + 1
+                endy = starty + 3
+            for i in range(startx, endx, 1):
+                for j in range(starty, endy, 1):
+                        temp_arr = self.coordinateDomains[str(i) + str(j)]
+                        temp_arr.remove(coord_value)
+                        self.coordinateDomains[str(i) + str(j)] = temp_arr
     def backwardCheck(self):
         if self.noEmptySpaces():
             return True
-        coord = self.chooseNextCorrdinate()
+        coord = self.chooseNextCoordinate()
         for i in range(1,10):
             if i in self.coordinateDomains[str(coord[0])+str(coord[1])]:
                 self.coordinateValue[coord[0]][coord[1]] = i
@@ -60,9 +78,12 @@ class sudokuPuzzle():
         return False
     
     def chooseNextCoordinate(self):
+        coord = ""
         #MRV heuristic
         #Degree heuristic
-        return
+        return coord
+    def noEmptySpaces(self):
+        return 
     def hyperSudoku(self):
         return
     def forwardChecking(self):
@@ -71,12 +92,14 @@ class sudokuPuzzle():
         return
     
 
-input_file = open("Sample_Input.txt", "r")
-coordinateArray = []
-for line in input_file:
-    temp_num_list = line.strip().split()
-    coordinateArray.append(temp_num_list)
+i1= open("Input1.txt", "r")
+o1 = open("Output1.txt", "w+")
 
+solver = SudokuPuzzle()
+solver.create_puzzle(i1, o1)
+solver.print_solution()
+i1.close()
+o1.close()
 #go through text file
 #insert in dictionary all the coordinates and the default [1-9] domain list
 #Go back to coordinates with pre-set values, run forward checking with them
